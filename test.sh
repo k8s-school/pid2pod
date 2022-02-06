81743ee9eda334b4707bcd73b715ffe20ca0827e
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2022-02-06 14:41:14 +0100

    upd

--- test.sh
+++ test.sh
@@ -4,8 +4,10 @@ set -euxo pipefail
 
 go build .
 docker cp pid2pod kind-control-plane:/
-docker exec -it kind-control-plane /pid2pod -p 1426 
+docker exec -it kind-control-plane /pid2pod -p 990
+echo
+docker exec -it kind-control-plane /pid2pod -p 1426
 echo
 docker exec -it kind-control-plane /pid2pod -p 1995
-echo 
+echo
 docker exec -it kind-control-plane /pid2pod -p 376
